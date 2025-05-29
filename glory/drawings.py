af1af7dc80f1927e7160cf25e426a0ed3e17ba2a
Algebra388 <algebra388@gmail.com>
2025-05-29 11:46:56 +0800

    initialize the env of drone

--- glory/drawings.py
+++ glory/drawings.py
@@ -2,11 +2,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import ast
 
-file_name_vdn = '2025-05-20 14_00_monster_lasthp_vdn_env_baseline.txt'
+file_name_vdn = '2025-05-26 13_08_monster_lasthp_vdn_env_baseline.txt'
 
-length = 4000
+length = 8000
 
-def smoothing(data, window_size = 20):
+def smoothing(data, window_size = 25):
     smoothed_data = []
     for i in range(len(data)):
         start = max(0, i - window_size)
@@ -18,23 +18,42 @@ def smoothing(data, window_size = 20):
 with open(file_name_vdn, 'r') as f:
     data = f.read()
     y_vdn_base = smoothing(np.array(ast.literal_eval(data))) # get list
+
+    print(f"len(y_vdn_base) = {len(y_vdn_base)}")
+
     y_vdn_base = y_vdn_base[:length]
     x_vdn_base = np.array(range(len(y_vdn_base)))
 
-file_name_qmix = '2025-05-22 14_27_monster_lasthp_qmix_env_baseline.txt'
+file_name_qmix = '2025-05-26 13_37_monster_lasthp_qmix_env_baseline.txt'
 with open(file_name_qmix, 'r') as f:
     data = f.read()
     y_qmix_base = smoothing(np.array(ast.literal_eval(data))) # get list
+    
+    print(f"len(y_qmix_base) = {len(y_qmix_base)}")
+
     y_qmix_base = y_qmix_base[:length]
     x_qmix_base = np.array(range(len(y_qmix_base)))
 
 
-file_name_vdn_promotion = '2025-05-21 17_13_monster_lasthp_vdn_env_promotion.txt'
+file_name_vdn_promotion = '2025-05-27 14_29_monster_lasthp_vdn_env_promotion.txt'
 with open(file_name_vdn_promotion, 'r') as f:
     data = f.read()
     y_vdn_promotion = smoothing(np.array(ast.literal_eval(data)))
+
+    print(f"len(y_vdn_promotion) = {len(y_vdn_promotion)}")
+
     y_vdn_promotion = y_vdn_promotion[:length]
     x_vdn_promotion = np.array(range(len(y_vdn_promotion)))
+
+file_name_qmix_promotion = '2025-05-28 13_32_monster_lasthp_qmix_env_promotion.txt'
+with open(file_name_qmix_promotion, 'r') as f:
+    data = f.read()
+    y_qmix_promotion = smoothing(np.array(ast.literal_eval(data)))
+
+    print(f"len(y_vdn_promotion) = {len(y_vdn_promotion)}")
+
+    y_qmix_promotion = y_qmix_promotion[:length]
+    x_qmix_promotion = np.array(range(len(y_qmix_promotion)))
 
 plt.figure(figsize = (10, 6))
 
@@ -42,7 +61,7 @@ def plot_with_std(x, y, std, label, color):
     plt.plot(x, y, label = label, color = color)
     plt.fill_between(x, y - std, y + std, color = color, alpha = 0.3)
 
-def rolling_std(data, window = 50):
+def rolling_std(data, window =50):
     std = []
     for i in range(len(data)):
         start = max(0, i - window)
@@ -53,6 +72,7 @@ def rolling_std(data, window = 50):
 plot_with_std(x_vdn_base, y_vdn_base, rolling_std(y_vdn_base), 'VDN baseline', 'blue')
 plot_with_std(x_qmix_base, y_qmix_base, rolling_std(y_qmix_base), 'QMIX baseline', 'orange')
 plot_with_std(x_vdn_promotion, y_vdn_promotion, rolling_std(y_vdn_promotion), 'VDN promotion', 'green')
+plot_with_std(x_qmix_promotion, y_qmix_promotion, rolling_std(y_qmix_promotion), 'QMIX promotion', 'red')
 
 plt.xlabel('Episode')
 plt.ylabel('Monster HP')
